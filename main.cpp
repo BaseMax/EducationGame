7ae728a8bd753c70d019a939590213c21103eb84
Max Base <MaxBaseCode@Gmail.Com>
2019-06-25 17:58:32 +0430

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+*
+* @Name : EducationGame/main.cpp
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2019-06-25
+* @Released under : https://github.com/BaseMax/EducationGame/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/EducationGame
+*
+**/
 #include <iostream>
 #include <cstdlib>
 #include <cstdio>
